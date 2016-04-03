558a4f2847455041c03fdb1af1aef37d117fe65e
Patrick Golden <ptgolden0@gmail.com>
2016-04-03 12:16:52 -0400

    Second stab
      * No use of tables
    
      * Notion of "trending" pairs which are highlighted in green

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,12 +7,6 @@ var React = require('react')
 
 const AUTHORS_BY_MSG_ID = {}
 
-var communications = Immutable.OrderedMap()
-
-
-var ct = 0;
-
-
 function addCommunication(communicationsMap, msg) {
   let author = Immutable.fromJS(msg.from[0])
     , inReplyTo = (msg.inReplyTo || [])[0]
@@ -26,7 +20,10 @@ function addCommunication(communicationsMap, msg) {
     pair = Immutable.List([author, AUTHORS_BY_MSG_ID[inReplyTo]]);
   }
 
-  return communicationsMap.update(pair, 0, v => v + 1)
+  return communicationsMap.update(
+    pair,
+    Immutable.Map({ total: 0, trend: 0 }),
+    v => v.update('total', n => n + 1).update('trend', n => n + 1))
 }
 
 module.exports = React.createClass({
@@ -38,14 +35,19 @@ module.exports = React.createClass({
   getInitialState() {
     return {
       numMessages: 0,
-      communications
+      communications: null,
     }
   },
 
   componentDidMount() {
-    var updateState = () => this.setState({
+    var i = 0
+      , communications = Immutable.OrderedMap()
+      , updateState
+
+
+    updateState = () => this.setState({
       communications,
-      numMessages: communications.reduce((acc, ct) => acc + ct, 0)
+      numMessages: communications.reduce((acc, counts) => acc + counts.get('total'), 0)
     });
 
     http.get('./public-whatwg-archive.mbox', res => res.pipe(mboxParser));
@@ -54,12 +56,20 @@ module.exports = React.createClass({
       if (!msg.from) return;
 
       communications = addCommunication(communications, msg);
+      i += 1;
 
-      if (communications.size % 5 === 0) {
+      if (i % 4 === 0) {
         mboxParser.pause();
-        setTimeout(() => mboxParser.resume(), 40);
+        setTimeout(() => mboxParser.resume(), 50);
         updateState();
       }
+
+      if (i % 16 === 0) {
+        communications = communications.map(counts => (
+          counts.update('trend', n => n <= 1 ? 0 : n - 1)
+        ));
+      }
+
     });
 
     mboxParser.on('end', updateState);
@@ -69,61 +79,42 @@ module.exports = React.createClass({
     var { numMessages, communications } = this.state
       , sortedCommunications
 
-    sortedCommunications = communications
-      .sort((a, b) => a === b ? 0 : b > a ? 1 : -1)
+    sortedCommunications = (communications || Immutable.OrderedMap())
+      .sort((a, b) => {
+        a = a.get('total');
+        b = b.get('total');
+
+        return a === b ? 0 : b > a ? 1 : -1
+      })
       .toKeyedSeq()
       .map((ct, pair) => (
-        <tr key={ pair.hashCode() }>
-          <td>{ ct }</td>
-          <td>{ pair.getIn([0, 'name']) }</td>
-          <td>{ pair.getIn([1, 'name']) || "(nobody)" }</td>
-        </tr>
+        <div key={ pair.hashCode() } className="px1" style={{
+          background: `rgba(40, 255, 40, ${ct.get('trend') / 8})`
+        }}>
+          <span className="inline-block bold border-box" style={{
+            width: 42,
+            textAlign: 'right',
+            paddingRight: 8
+          }}>{ ct.get('total') }</span>
+          { pair.getIn([0, 'name']) }
+          {" ➝ "}
+          { pair.getIn([1, 'name']) || "(nobody)" }
+        </div>
       ))
 
     return (
       <div>
         <h2>{ numMessages } messages</h2>
-        <div className="m3 flex" style={{ justifyContent: "space-between" }}>
-          <table>
-            <thead>
-              <tr>
-                <th>Count</th>
-                <th>From</th>
-                <th>To</th>
-              </tr>
-            </thead>
-            <tbody>
-              { sortedCommunications.take(25).toArray() }
-            </tbody>
-          </table>
-
-          <table className="bg-green">
-            <thead>
-              <tr>
-                <th>Count</th>
-                <th>From</th>
-                <th>To</th>
-              </tr>
-            </thead>
-            <tbody>
-              { sortedCommunications.skip(25).take(25).toArray() }
-            </tbody>
-          </table>
-
-          <table>
-            <thead>
-              <tr>
-                <th>Count</th>
-                <th>From</th>
-                <th>To</th>
-              </tr>
-            </thead>
-            <tbody>
-              { sortedCommunications.skip(50).take(25).toArray() }
-            </tbody>
-          </table>
-
-        </div>
+        {
+          [0, 25, 50].map(skip =>
+            <div
+                key={skip}
+                className="left border-box px1"
+                style={{ width: '33%' }}>
+              { sortedCommunications.skip(skip).take(25).toArray() }
+            </div>
+          )
+        }
       </div>
     )
   }
